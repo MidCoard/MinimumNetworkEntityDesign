1f29832704f1034be86b3cb27e80e921aa080247
MidCoard <2624646185@qq.com>
2022-04-18 16:44:18 +0800

    Init commit

---
+++ layers/LinkLayer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 周蜀杰 on 2022/4/18.
+//
+
+#include "LinkLayer.h"
---
+++ layers/LinkLayer.h
@@ -0,0 +1,15 @@
+//
+// Created by 周蜀杰 on 2022/4/18.
+//
+
+#ifndef NETWORKDESIGN_LINKLAYER_H
+#define NETWORKDESIGN_LINKLAYER_H
+
+
+class LinkLayer {
+
+
+};
+
+
+#endif //NETWORKDESIGN_LINKLAYER_H
